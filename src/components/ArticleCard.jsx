bb7c8de437b78c82260eba71a51571ca872deecf
Anurag Guha <rick.anurag2@gmail.com>
2023-10-08 02:42:48 +0530

    Fixed Bugs

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -10,7 +10,7 @@ export default function ArticleCard(props){
         <>
             <div className="container">
                 {props.articleRef && <div className="row">
-                    <img src={props.articleRef} className="article-img"></img>
+                    <img src={"/src/assets/article-one.png"} className="article-img"></img>
                 </div>}
                 <div className="row article-category">
                     <div>{props.articleCategory}</div>
